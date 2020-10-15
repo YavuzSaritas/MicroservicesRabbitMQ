f32567c1e352db0b1bfd7d1f3b5efec1a9f6c7f8
yavuz.saritas <yavuz.saritas@demirorenteknoloji.com>
2020-10-15 15:58:31 +0300

    Producer and Consumer

---
+++ ProducerConsumerRabbitMQ/Producer/Sender.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Dynamic;
+using System.Text;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var conn = factory.CreateConnection())
+            using (var channel = conn.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                var message = "Getting Started with .Net Core RabbitQ";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", "BasicTest", null, body);
+                Console.WriteLine($"Sent Message...... {message}");
+            }
+            Console.WriteLine("Press [enter] to exit the Sender App...");
+            Console.ReadLine();
+        }
+    }
+}
